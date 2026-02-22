d2631fab6f7f32fbf8fda8336ff338b5571e6593
AliButtCS30 <muhammadalibutt463@gmail.com>
2026-02-22 23:25:41 +0500

    week 2 tasks

---
+++ Week2/century.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+main()
+
+{  
+cout<<"                                          ooo    ooo                                      "<<endl;
+cout<<"                                         o   o  o   o                                     "<<endl; 
+cout<<"                                           oo   o   o                                     "<<endl;
+cout<<"          .......                         o     o   o                    .........        "<<endl;
+cout<<"          .......                         oooo   ooo  th                ..........        "<<endl;
+cout<<"          ........                    ...................              ..........         "<<endl;
+cout<<"            ........                     C E N T U R Y                   .........                 "<<endl;
+cout<<"              .......                 ...................              ........                   "<<endl;  
+cout<<"                 .....                 ooo ooo o   o ooo              .....               "<<endl;                 
+cout<<"                   .....                o  o    o o   o              .....                "<<endl;                   
+cout<<"                       ...              o  ooo   o    o              ...                  "<<endl;                        
+cout<<"                       __||__           o  o    o o   o           __||__                  "<<endl;                                                    
+cout<<"                ______| ____ |___       o  ooo o   o  o     _____| ____ |____             "<<endl;
+
+}                     
---
+++ Week2/gun.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+          system("color 02");
+cout<<"              +__^---------,-------,-----,---^--,   "<<endl;
+cout<<"              |  |||||||| '_______'  |         O    "<<endl; 
+cout<<"              '+---------------------^---------|    "<<endl;
+cout<<"                '_______ _______ ____________       "<<endl;
+cout<<"                '/ xxxxx'/'|    '/'         '        "<<endl;
+cout<<"                / xxxxx /  |    /'                   "<<endl;
+cout<<"               / xxxxx /'------,                      "<<endl;
+cout<<"              / xxxxx /                              "<<endl;
+cout<<"             (-------(                              "<<endl;
+cout<<"              '-----'                              "<<endl;
+cout<<"                                                    "<<endl;
+}
