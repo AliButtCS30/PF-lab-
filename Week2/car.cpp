4f3fdec479b32757f1874aa93b62b0ba7e3af9f2
AliButtCS30 <muhammadalibutt463@gmail.com>
2026-03-01 17:29:39 +0500

    week2 updated

---
+++ Week2/car.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+  
+cout<<"                              _____________         "<<endl;
+cout<<"                                //  ||\\ \\           "<<endl;
+cout<<"                          _____//___||_\\ \\___      "<<endl;
+cout<<"                          )  _          _    \\           "<<endl;
+cout<<"                          |_/ \\________/ \\___|           "<<endl;
+cout<<"                         ___\\_/________\\_/___           "<<endl;
+}
